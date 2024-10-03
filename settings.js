906478e3f7ce24da531c1a619d63d798e0e93b86
Andy Krohg <akrohg@redhat.com>
2024-10-03 07:37:46 -0400

    Make this thing run in a container

--- settings.js
+++ settings.js
@@ -1,19 +1,19 @@
 export default 
 {
-    "minecraft_version": "1.20.4", // supports up to 1.20.4
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916,
+    "minecraft_version": "1.19", // supports up to 1.20.4
+    "host": process.env.MINECRAFT_SERVER_HOST || "localhost", //minecraft server host 
+    "port": process.env.MINECRAFT_SERVER_PORT || 8080, //minecraft server port
     "auth": "offline", // or "microsoft"
     
     "profiles": [
-        "./andy.json",
+        `./${process.env.PROFILE || "llama"}.json`,
         // add more profiles here, check ./profiles/ for more
         // more than 1 profile will require you to /msg each bot indivually
     ],
     "load_memory": false, // load memory from previous session
     "init_message": "Say hello world and your name", // sends to all on spawn
 
-    "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
+    "allow_insecure_coding": true, // allows newAction command and model can write/run code on your computer. enable at own risk
     "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
     
     "max_commands": -1, // max number of commands to use in a response. -1 for no limit
--- src/agent/prompter.js
+++ src/agent/prompter.js
@@ -21,7 +21,7 @@ export class Prompter {
 
         let name = this.profile.name;
         let chat = this.profile.model;
-        let url = this.profile.url;
+        let url = process.env.MODEL_SERVER_ENDPOINT || this.profile.url;
         if (typeof chat === 'string' || chat instanceof String) {
             chat = {model: chat};
             if (chat.model.includes('gemini'))
